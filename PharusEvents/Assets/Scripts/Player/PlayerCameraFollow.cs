ec0a2f862c1aa66d4d3964bf61d01e0ab2b5c146
khaled elaadwy <46924819+kElaadwy@users.noreply.github.com>
2022-02-23 11:58:53 +0200

    New camera!
    the old system for vision is replaced with a new one.
    the new system works with multiplayer
    most of the issues are solved
    the new system has some problems with the movement direction

---
+++ PharusEvents/Assets/Scripts/Player/PlayerCameraFollow.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerCameraFollow : MonoBehaviour
+{
+    public static PlayerCameraFollow Instance { get; private set; }
+
+    private Cinemachine.CinemachineVirtualCamera cinemachineVirtualCamera;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        cinemachineVirtualCamera = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+    }
+
+    public void FollowPlayer(Transform transform)
+    {
+        cinemachineVirtualCamera.Follow = transform;
+    }
+
+}
